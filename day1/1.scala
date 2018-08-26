4bc50d37d24704a2633408ee6c77f05fefe48920
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-08-26 15:36:54 +0530

    Create 1.scala

---
+++ day1/1.scala
@@ -0,0 +1,32 @@
+val a=10
+// a: Int = 10
+
+var b=10
+// b: Int = 10
+
+b=b+10
+// b: Int = 210
+
+print(a)
+// 10
+
+print(a,b)
+// (10,210)
+
+a
+// res3: Int = 10
+
+b
+// res4: Int = 210
+
+var s="hello"
+// s: String = hello
+
+var f=3.14
+// f: Double = 3.14
+
+var s: String="hello"
+// s: String = hello
+
+var s:java.lang.String="hello"
+// s: String = hello
