57e6b16d621f49c6279562072172231059284047
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-08-26 15:45:57 +0530

    Create 4.scala

---
+++ day1/4.scala
@@ -0,0 +1,17 @@
+var price = 20
+// price: Int = 20
+
+var tax = 0.06
+// tax: Double = 0.06
+
+def grandtotal=price+price*tax
+// grandtotal: Double
+
+grandtotal
+// Double = 21.2
+
+price=200
+// price: Int = 200
+
+grandtotal
+// Double = 212.0
