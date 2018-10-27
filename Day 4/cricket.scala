2f87af1a42753d2d621789c385f3259520f738d5
Rutuja Rajesh <rutujarajesh23@gmail.com>
2018-10-27 09:28:27 +0530

    Update cricket.scala

--- Day 4/cricket.scala
+++ Day 4/cricket.scala
@@ -1,5 +1,5 @@
 import scala.io.Source
-val lines=Source.fromFile("335982.csv").getLines().toList
+val lines=Source.fromFile("cricket.csv").getLines().toList
 
 print(lines)
 var times=lines.filter(x=>x.contains("info,team")).map(x=>x.split(",")(2))
